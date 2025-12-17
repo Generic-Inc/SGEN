1d9e8e0b3d9246ca177a95a73af3c241c4a8bb6c
Ryan K <249889418+ryan-k-git@users.noreply.github.com>
2025-12-17 17:25:46 +0800

    Simple database manager

---
+++ src/global/dbmanager.py
@@ -0,0 +1,94 @@
+from typing import Optional
+from pathlib import Path
+import aiosqlite
+
+SCHEMA_PATH = "../database/schema.sql"
+
+class DBManager:
+    def __init__(self, path: str | Path=SCHEMA_PATH) -> None:
+        self.path = path
+        self.con: Optional[aiosqlite.Connection] = None
+
+    async def _create_tables(self, schema_path: str | Path=SCHEMA_PATH) -> None:
+        """
+        Creates tables in the database based on the schema in the schema path.
+        :param schema_path: path to the schema file
+        :return: None
+        """
+        con = self.con
+        with open(schema_path, "r", encoding='utf-8') as f:
+            schema = f.read()
+        await con.executescript(schema)
+        await con.commit()
+
+    async def fetch_one(self, query: str, values: tuple=None) -> tuple:
+        """
+        Fetches one row of the database based on the query and the values
+        :param query: query to fetch
+        :param values: values from query
+        :return: tuple
+        """
+        con = self.con
+        if values:
+            async with con.execute(query, values) as cur:
+                data = await cur.fetchone()
+        else:
+            async with con.execute(query) as cur:
+                data = await cur.fetchone()
+        return data
+
+    async def fetch_all(self, query: str, values: tuple = None) -> list[tuple]:
+        """
+        fetches all rows of the database based on the query and the values
+        :param query: query to fetch
+        :param values: values from query
+        :return: list[tuple]
+        """
+        con = self.con
+        if values:
+            async with con.execute(query, values) as cur:
+                data = await cur.fetchone()
+        else:
+            async with con.execute(query) as cur:
+                data = await cur.fetchone()
+        return data
+
+    async def execute(self, query: str, values: tuple = None, commit=True) -> None:
+        """
+        Execute an SQL query in the database based on the query and the values
+        :param query: query to execute
+        :param values: values from query
+        :param commit: whether to commit the query
+        :return: None
+        """
+        con = self.con
+        await con.execute(query, values)
+        if commit:
+            await con.commit()
+
+    async def executescript(self, query: str) -> None:
+        """
+        execute an SQL script
+        :param query: query to execute
+        :return: None
+        """
+        con = self.con
+        await con.executescript(query)
+        await con.commit()
+
+    async def commit(self) -> None:
+        """
+        commit to the database
+        :return: None
+        """
+        con = self.con
+        await con.commit()
+
+    async def initialize(self) -> None:
+        """
+        initialize the database
+        :return: None
+        """
+        self.con = await aiosqlite.connect(self.path)
+        await self._create_tables(self.path)
+

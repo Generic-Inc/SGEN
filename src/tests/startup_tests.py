12cd2e9460b8f421362d2fca8ffa1e33e35ddb60
Ryan <249889418+ryan-k-git@users.noreply.github.com>
2026-01-21 11:39:36 +0800

    config test

--- src/tests/startup_tests.py
+++ src/tests/startup_tests.py
@@ -2,6 +2,7 @@ import asyncio
 import json
 from pprint import pprint
 
+from config.config import CONFIG
 from global_src.db import DATABASE
 
 
@@ -17,6 +18,9 @@ async def main():
         print(f"Schema for table '{table_name}': {json.dumps(schema)}")
         rows = await DATABASE.fetch_all(f"SELECT * FROM {table_name} LIMIT 5;")
         pprint(f"Sample data from table '{table_name}': {json.dumps(rows)}")
+    print("*" * 20)
+    config_stuff = await CONFIG.load_config()
+    print(config_stuff)
 
 if __name__ == "__main__":
     asyncio.run(main())

c8d2716b4cb409e3d82fc97b44d6ed207e9e2b5b
Ryan <249889418+ryan-k-git@users.noreply.github.com>
2026-01-25 18:27:44 +0800

    path change

--- src/server/app.py
+++ src/server/app.py
@@ -3,17 +3,15 @@ import os
 import threading
 
 from flask import Flask, render_template
+
+from global_src.constants import TEMPLATES_PATH, STATIC_PATH
 from routing.api import api
 from global_src.db import DATABASE
 from config.config import CONFIG
 
-base_dir = os.path.dirname(os.path.abspath(__file__))
-client_folder = os.path.abspath(os.path.join(base_dir, "..", "client"))
-template_folder = os.path.join(client_folder, "templates")
-static_folder = os.path.join(client_folder, "static")
 app = Flask(__name__,
-            template_folder=template_folder,
-            static_folder=static_folder)
+            template_folder=TEMPLATES_PATH,
+            static_folder=STATIC_PATH)
 
 app.json.sort_keys = False
 app.register_blueprint(api, url_prefix='/api')
@@ -33,4 +31,4 @@ if __name__ == '__main__':
     thread = threading.Thread(target=main, daemon=True)
     thread.start()
     app.run(use_reloader=False, debug=True)
-    print(f"Server running. Templates at: {template_folder}")
+    print(f"Server running. Templates at: {TEMPLATES_PATH}")
--- src/server/global_src/constants.py
+++ src/server/global_src/constants.py
@@ -1,8 +1,15 @@
 from pathlib import Path
 
-CONFIG_FOLDER_PATH = Path(__file__).parent.parent/ "config"
-DATABASE_FOLDER_PATH = Path(__file__).parent.parent / "database"
+SRC_PATH = Path(__file__).parent.parent.parent
+SERVER_PATH = SRC_PATH / "server"
+CLIENT_PATH = SERVER_PATH / "client"
+
+CONFIG_FOLDER_PATH = SERVER_PATH / "config"
+DATABASE_FOLDER_PATH = SERVER_PATH / "database"
 
 CONFIG_PATH: Path = CONFIG_FOLDER_PATH / "config.json"
 SCHEMA_PATH = DATABASE_FOLDER_PATH / "schema.sql"
 DATABASE_PATH: Path = DATABASE_FOLDER_PATH / "app.db"
+
+STATIC_PATH = CLIENT_PATH / "static"
+TEMPLATES_PATH = CLIENT_PATH / "templates"

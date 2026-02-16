ed45cd72ac4f211f55dfed2f8afeea2608d71a86
Ryan <249889418+ryan-k-git@users.noreply.github.com>
2026-02-16 15:13:31 +0800

    search back

--- src/server/routing/api/main.py
+++ src/server/routing/api/main.py
@@ -16,5 +16,9 @@ async def search():
         data = request.json
         query = data.get("content", "")
         community_get = await DATABASE.fetch_all("SELECT community_id, display_name FROM communities WHERE display_name LIKE ? LIMIT 3", (f"%{query}%",))
-        events_get = await DATABASE.fetch_all("SELECT event_id, event_name FROM events WHERE event_name LIKE ?", (f"%{query}%",))
-        return {"results": [{"name": f"community:{i[1]}", "href": f"community/{i[0]}"} for i in community_get] + [{"name": f"event:{i[1]}", "href": f"community/1/event/{i[0]}"} for i in events_get]}
+        events_get = await DATABASE.fetch_all("SELECT event_id, event_name FROM events WHERE event_name LIKE ? LIMIT 3", (f"%{query}%",))
+        users_get = await DATABASE.fetch_all("SELECT user_id, display_name FROM users WHERE display_name LIKE ? LIMIT 3", (f"%{query}%",))
+        return {"results": [{"name": f"community:{i[1]}", "href": f"community/{i[0]}"} for i in community_get] +
+                           [{"name": f"event:{i[1]}", "href": f"community/1/event/{i[0]}"} for i in events_get] +
+        [{"name": f"user:{i[1]}", "href": f"user/{i[0]}"} for i in users_get]
+                }

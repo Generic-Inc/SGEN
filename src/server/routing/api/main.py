d337f74d91d3d7c66bedd4dbe814e793045a4a60
Ryan <249889418+ryan-k-git@users.noreply.github.com>
2026-02-16 14:56:41 +0800

    search back

--- src/server/routing/api/main.py
+++ src/server/routing/api/main.py
@@ -1,11 +1,20 @@
-from flask import Blueprint
-from . import api, search_blueprint
+from flask import Blueprint, request
+
+from global_src.db import DATABASE
+from . import api
 
 
 @api.route('/ping')
 def ping():
     return {"message": "pong!"}
 
-@api.route("/search")
+@api.route("/search", methods=["GET", "POST"])
 async def search():
-    return {"option": [{"name": "event:test", "href": "community/1/event/1"}, {"name": "community:test", "href": "community/1"}]}
+    if request.method == "GET":
+        return {"option": [{"name": "event:test", "href": "community/1/event/1"}, {"name": "community:test", "href": "community/1"}]}
+    elif request.method == "POST":
+        data = request.json
+        query = data.get("content", "")
+        community_get = await DATABASE.fetch_all("SELECT community_id, display_name FROM communities WHERE display_name LIKE ? LIMIT 3", (f"%{query}%",))
+        events_get = await DATABASE.fetch_all("SELECT event_id, event_name FROM events WHERE event_name LIKE ?", (f"%{query}%",))
+        return {"results": [{"name": f"community:{i[1]}", "href": f"community/{i[0]}"} for i in community_get] + [{"name": f"event:{i[1]}", "href": f"community/1/event/{i[0]}"} for i in events_get]}
